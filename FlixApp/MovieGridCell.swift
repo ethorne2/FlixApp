ed783300ff66ef5cc593e4b21d1e48eaecac79ad
Elizabeth Thorne <66274014+ethorne2@users.noreply.github.com>
2021-10-01 22:16:35 -0500

    Part 2 of Flix App
    add on for part 2 of flix app project

---
+++ FlixApp/MovieGridCell.swift
@@ -0,0 +1,14 @@
+//
+//  MovieGridCell.swift
+//  FlixApp
+//
+//  Created by Elizabeth Thorne on 10/1/21.
+//
+
+import UIKit
+
+class MovieGridCell: UICollectionViewCell {
+    
+    @IBOutlet weak var posterView: UIImageView!
+    
+}
